7869d00b18796d89f80b04a57a57ba509a26570a
TeacherCangNulling <3218870799@qq.com>
2018-09-02 11:23:40 +0800

    前端js编写

---
+++ src/main/webapp/resourcecs/js/shop/shopoperation.js
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+	$(function(){
+		
+		alert("加载js完成");
+		
+		var initUrl = '/CampusShop/shopadmin/getshopinitinfo';
+		var registerShopUrl ='/CampusShop/shopadmin/registershop';
+		
+		alert(initUrl);
+		getShopInitInfo();
+		
+		function getShopInitInfo(){
+			$.getJSON(initUrl,function(data){
+				if(data.success){
+					var tempHtml = '';
+					var tempAreaHtml = '';
+					data.shopCategoryList.map(function(item,index){
+						temoHtml+='<option data-id="'+item.shopCategoryId +'">'
+						+item.shopCategroryName +'</option>';
+					});
+					data.areaList.map(function(item,index){
+						temoAreaHtml+='<option data-id="'+item.areaId +'">'
+						+item.areaName +'</option>';
+					});
+					$('#shop-category').html(tempHtml);
+					$('#area').html(tempAreaHtml);
+					
+				}
+				
+			});
+			
+			
+			$("#submit").click(function(){
+				var shop = {};
+				shop.shopName=$('#shop-name').val();
+				shop.shopAddr=$('#shop-addr').val();
+				shop.phone=$('#shop-phone').val();
+				shop.shopDesc=$('#shop-desc').val();
+				shop.shopCategory={
+						shopCategoryId:$('#shop-category').find('option').not(function(){
+							return !this.selected;
+						}).data('id')
+				};
+				shop.area={
+						areaId:$('#area').find('option').not(function(){
+							return !this.selected;
+						}).data('id')
+				};
+				
+				var shopImg = $("#shop-img")[0].files[0];
+				var formData = new FormData();
+				formData.append("shopImg", shopImg);
+				formData.append("shopStr", JSON.stringify(shop));
+				$.ajax({
+					url:registerShopUrl,
+					type:'POST',
+					data:formData,
+					contentType:false,
+					proceesData:false,
+					cache:false,
+					success:function(data){
+						if(data.success){
+							$.toast('提交成功');
+						}else{
+							$.toast('提交失败！'+data.errMsg);
+						}
+					}
+				});
+				
+				
+			});
+			
+			
+			
+			
+			
+		}
+	});
