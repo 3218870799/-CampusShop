cdab7faaa7945218485d8465d46332d7818f78ed
TeacherCangNulling <3218870799@qq.com>
2018-09-06 14:02:13 +0800

    添加注解

--- src/main/java/com/xqc/campusshop/dao/ShopCategoryDao.java
+++ src/main/java/com/xqc/campusshop/dao/ShopCategoryDao.java
@@ -6,8 +6,17 @@ import org.apache.ibatis.annotations.Param;
 
 import com.xqc.campusshop.entity.ShopCategory;
 
+/**
+ * 店铺类别数据访问层接口
+ * @author A Cang（xqc）
+ *
+ */
 public interface ShopCategoryDao {
-	
+	/**
+	 * 查询店铺类别
+	 * @param shopCategory
+	 * @return
+	 */
 	List<ShopCategory> queryShopCategory(@Param("shopCategoryCondition")
 	ShopCategory shopCategory);
 
