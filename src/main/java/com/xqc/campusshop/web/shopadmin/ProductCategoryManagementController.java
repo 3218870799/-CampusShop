0faa26a6e3e29b674bad3938f50d62683ebd4bd4
TeacherCangNulling <3218870799@qq.com>
2018-09-06 14:03:45 +0800

    添加商品类别Controller层

--- src/main/java/com/xqc/campusshop/web/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/com/xqc/campusshop/web/shopadmin/ProductCategoryManagementController.java
@@ -1,15 +1,19 @@
 package com.xqc.campusshop.web.shopadmin;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.xqc.campusshop.dto.ProductCategoryExecution;
 import com.xqc.campusshop.dto.Result;
 import com.xqc.campusshop.entity.ProductCategory;
 import com.xqc.campusshop.entity.Shop;
@@ -53,7 +57,11 @@ public class ProductCategoryManagementController {
 		}
 		return modelMap;
 	}*/
-	
+	/**
+	 * 获取类别列表，返回json格式
+	 * @param request
+	 * @return
+	 */
 	@RequestMapping(value = "/getproductcategorylist", method = RequestMethod.GET)
 	@ResponseBody
 	private Result<List<ProductCategory>> getProductCategoryList(HttpServletRequest request){
@@ -74,6 +82,46 @@ public class ProductCategoryManagementController {
 			return new Result<List<ProductCategory>>(false,ps.getState(),ps.getStateInfo());
 		}
 		
+	}
+	/**
+	 * 批量添加商品类别，返回json格式
+	 * @param productCategoryList
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/addproductcategorys", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> addProductCategorys(
+			@RequestBody List<ProductCategory> productCategoryList,
+			HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		Shop currentShop = (Shop) request.getSession().getAttribute(
+				"currentShop");
+		for (ProductCategory pc : productCategoryList) {
+			pc.setShopId(currentShop.getShopId());
+		}
+		if (productCategoryList != null && productCategoryList.size() > 0) {
+			try {
+				ProductCategoryExecution pe = productCategoryService
+						.batchAddProductCategory(productCategoryList);
+				if (pe.getState() == ProductCategoryStateEnum.SUCCESS
+						.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pe.getStateInfo());
+				}
+			} catch (RuntimeException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少输入一个商品类别");
+		}
+		return modelMap;
 	}
 	
 	
