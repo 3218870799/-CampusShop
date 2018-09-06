26774a5ee5dbf6a6f3da1f447a47a3a4fe635890
TeacherCangNulling <3218870799@qq.com>
2018-09-06 15:08:54 +0800

    删除商品类别Controller

--- src/main/java/com/xqc/campusshop/web/shopadmin/ProductCategoryManagementController.java
+++ src/main/java/com/xqc/campusshop/web/shopadmin/ProductCategoryManagementController.java
@@ -124,7 +124,43 @@ public class ProductCategoryManagementController {
 		return modelMap;
 	}
 	
-	
+	/**
+	 * 删除商品类别
+	 * @param productCategoryId
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/removeproductcategory", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> removeProductCategory(Long productCategoryId,
+			HttpServletRequest request) {
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		if (productCategoryId != null && productCategoryId > 0) {
+			try {
+				Shop currentShop = (Shop) request.getSession().getAttribute(
+						"currentShop");
+				ProductCategoryExecution pe = productCategoryService
+						.deleteProductCategory(productCategoryId,
+								currentShop.getShopId());
+				if (pe.getState() == ProductCategoryStateEnum.SUCCESS
+						.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pe.getStateInfo());
+				}
+			} catch (RuntimeException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请至少选择一个商品类别");
+		}
+		return modelMap;
+	}
 	
 	
 	
