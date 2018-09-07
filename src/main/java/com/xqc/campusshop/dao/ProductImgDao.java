6b7274b52479ca8979fc74f3218853187cc5cfd1
TeacherCangNulling <3218870799@qq.com>
2018-09-07 20:30:18 +0800

    商品图片数据访问层

---
+++ src/main/java/com/xqc/campusshop/dao/ProductImgDao.java
@@ -0,0 +1,21 @@
+package com.xqc.campusshop.dao;
+
+import java.util.List;
+
+import com.xqc.campusshop.entity.ProductImg;
+
+/**
+ * 商品图片数据访问层
+ * @author A Cang（xqc）
+ *
+ */
+public interface ProductImgDao {
+
+	/**
+	 * 批量添加商品详情图片
+	 * @param productImgList
+	 * @return
+	 */
+	int batchInsertProductImg(List<ProductImg> productImgList);
+
+}
