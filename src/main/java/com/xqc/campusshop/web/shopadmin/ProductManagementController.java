b9aab284e822855dac056c7c2b6061944fa8bf7b
TeacherCangNulling <3218870799@qq.com>
2018-09-09 11:01:16 +0800

    商品修改Controller

--- src/main/java/com/xqc/campusshop/web/shopadmin/ProductManagementController.java
+++ src/main/java/com/xqc/campusshop/web/shopadmin/ProductManagementController.java
@@ -165,8 +165,11 @@ public class ProductManagementController {
 	@ResponseBody
 	private Map<String, Object> getProductById(@RequestParam Long productId) {
 		Map<String, Object> modelMap = new HashMap<String, Object>();
+		//空值判断
 		if (productId > -1) {
+			//获取商品信息
 			Product product = productService.getProductById(productId);
+			//获取该店铺下的商品类别列表
 			List<ProductCategory> productCategoryList = productCategoryService
 					.getByShopId(product.getShop().getShopId());
 			modelMap.put("product", product);
@@ -180,7 +183,82 @@ public class ProductManagementController {
 	}
 	
 	
-	
+	@RequestMapping(value = "/modifyproduct", method = RequestMethod.POST)
+	@ResponseBody
+	private Map<String, Object> modifyProduct(HttpServletRequest request) {
+		//是商品编辑时候调用还是上下架操作的时候调用
+		//若为前者则进行验证码判断，后者则跳过验证码判断
+		boolean statusChange = HttpServletRequestUtil.getBoolean(request,
+				"statusChange");
+		
+		Map<String, Object> modelMap = new HashMap<String, Object>();
+		//验证码判断
+		if (!statusChange && !CodeUtil.checkVerifyCode(request)) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "输入了错误的验证码");
+			return modelMap;
+		}
+		//接受前端参数的变量初始化，包括商品，缩略图，详情图列表实体类
+		ObjectMapper mapper = new ObjectMapper();
+		Product product = null;
+		String productStr = HttpServletRequestUtil.getString(request,
+				"productStr");
+		MultipartHttpServletRequest multipartRequest = null;
+		CommonsMultipartFile thumbnail = null;
+		List<CommonsMultipartFile> productImgs = new ArrayList<CommonsMultipartFile>();
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+				request.getSession().getServletContext());
+		//若请求中存在文件，取出相关文件（包括缩略图与详情图
+		if (multipartResolver.isMultipart(request)) {
+			multipartRequest = (MultipartHttpServletRequest) request;
+			thumbnail = (CommonsMultipartFile) multipartRequest
+					.getFile("thumbnail");
+			for (int i = 0; i < IMAGEMAXCOUNT; i++) {
+				CommonsMultipartFile productImg = (CommonsMultipartFile) multipartRequest
+						.getFile("productImg" + i);
+				if (productImg != null) {
+					productImgs.add(productImg);
+				}
+			}
+		}
+		try {
+			
+			//尝试获取前端传过来的表单String并将其转换成Product实体类
+			product = mapper.readValue(productStr, Product.class);
+		} catch (Exception e) {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", e.toString());
+			return modelMap;
+		}
+		if (product != null) {
+			try {
+				//从session中获取当前店铺的id并赋值给product，减少对前端数据的依赖
+				Shop currentShop = (Shop) request.getSession().getAttribute(
+						"currentShop");
+				Shop shop = new Shop();
+				shop.setShopId(currentShop.getShopId());
+				product.setShop(shop);
+				//开始进行商品信息更新操作
+				ProductExecution pe = productService.modifyProduct(product,
+						thumbnail, productImgs);
+				if (pe.getState() == ProductStateEnum.SUCCESS.getState()) {
+					modelMap.put("success", true);
+				} else {
+					modelMap.put("success", false);
+					modelMap.put("errMsg", pe.getStateInfo());
+				}
+			} catch (RuntimeException e) {
+				modelMap.put("success", false);
+				modelMap.put("errMsg", e.toString());
+				return modelMap;
+			}
+
+		} else {
+			modelMap.put("success", false);
+			modelMap.put("errMsg", "请输入商品信息");
+		}
+		return modelMap;
+	}
 	
 
 }
