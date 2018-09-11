d3fc1905783b5534e0bb02c8ee8fdeb29f85ef14
TeacherCangNulling <3218870799@qq.com>
2018-09-11 10:26:43 +0800

    头条状态枚举

---
+++ src/main/java/com/xqc/campusshop/enums/HeadLineStateEnum.java
@@ -0,0 +1,37 @@
+package com.xqc.campusshop.enums;
+
+/**
+ * 头条状态枚举
+ * @author A Cang（xqc）
+ *
+ */
+public enum HeadLineStateEnum {
+	SUCCESS(0, "创建成功"), INNER_ERROR(-1001, "操作失败"), EMPTY(-1002, "头条信息为空");
+
+	private int state;
+
+	private String stateInfo;
+
+	private HeadLineStateEnum(int state, String stateInfo) {
+		this.state = state;
+		this.stateInfo = stateInfo;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public static HeadLineStateEnum stateOf(int index) {
+		for (HeadLineStateEnum state : values()) {
+			if (state.getState() == index) {
+				return state;
+			}
+		}
+		return null;
+	}
+
+}
