f600bc1baf0d9874c43f92903d6fda6e18ead1ac
TeacherCangNulling <3218870799@qq.com>
2018-09-19 19:56:38 +0800

    区域Service接口

--- src/main/java/com/xqc/campusshop/service/AreaService.java
+++ src/main/java/com/xqc/campusshop/service/AreaService.java
@@ -2,10 +2,41 @@ package com.xqc.campusshop.service;
 
 import java.util.List;
 
+import com.xqc.campusshop.dto.AreaExecution;
 import com.xqc.campusshop.entity.Area;
 
 public interface AreaService {
-	
+	/**
+	 * 查询区域列表
+	 * @return
+	 */
 	List<Area> getAreaList();
+	/**
+	 * 添加区域信息
+	 * @param area
+	 * @return
+	 */
+	AreaExecution addArea(Area area);
+
+	/**
+	 * 修改区域信息
+	 * @param area
+	 * @return
+	 */
+	AreaExecution modifyArea(Area area);
+
+	/**
+	 * 删除区域
+	 * @param areaId
+	 * @return
+	 */
+	AreaExecution removeArea(long areaId);
+
+	/**
+	 * 批量删除
+	 * @param areaIdList
+	 * @return
+	 */
+	AreaExecution removeAreaList(List<Long> areaIdList);
 
 }
