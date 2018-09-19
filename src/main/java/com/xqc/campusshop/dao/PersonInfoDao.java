5c7a9201698333017bd86933a0a08afd0d7acd40
TeacherCangNulling <3218870799@qq.com>
2018-09-19 20:10:40 +0800

    个人信息数据访问层

--- src/main/java/com/xqc/campusshop/dao/PersonInfoDao.java
+++ src/main/java/com/xqc/campusshop/dao/PersonInfoDao.java
@@ -1,7 +1,16 @@
 package com.xqc.campusshop.dao;
 
-import com.xqc.campusshop.entity.PersonInfo;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.xqc.campusshop.entity.PersonInfo;
+/**
+ * 个人信息数据访问层
+ * 
+ * @author A Cang（xqc）
+ *
+ */
 public interface PersonInfoDao {
 
 	/**
@@ -10,6 +19,25 @@ public interface PersonInfoDao {
 	 * @return
 	 */
 	PersonInfo queryPersonInfoById(long userId);
+	
+	/**
+	 * 分页查询个人信息
+	 * @param personInfoCondition
+	 * @param rowIndex
+	 * @param pageSize
+	 * @return
+	 */
+	List<PersonInfo> queryPersonInfoList(
+			@Param("personInfoCondition") PersonInfo personInfoCondition,
+			@Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+	/**
+	 * 查询个人信息总数
+	 * @param personInfoCondition
+	 * @return
+	 */
+	int queryPersonInfoCount(
+			@Param("personInfoCondition") PersonInfo personInfoCondition);
 
 	/**
 	 * 添加用户信息
@@ -17,4 +45,17 @@ public interface PersonInfoDao {
 	 * @return
 	 */
 	int insertPersonInfo(PersonInfo personInfo);
+	/**
+	 * 修改个人信息
+	 * @param wechatAuth
+	 * @return
+	 */
+	int updatePersonInfo(PersonInfo personInfo);
+
+	/**
+	 * 删除个人信息
+	 * @param wechatAuth
+	 * @return
+	 */
+	int deletePersonInfo(long userId);
 }
