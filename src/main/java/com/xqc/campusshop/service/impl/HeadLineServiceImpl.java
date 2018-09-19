f25055e37390e3580c7147677ca12db3cf4d606b
TeacherCangNulling <3218870799@qq.com>
2018-09-19 20:06:40 +0800

    头条操作实现

--- src/main/java/com/xqc/campusshop/service/impl/HeadLineServiceImpl.java
+++ src/main/java/com/xqc/campusshop/service/impl/HeadLineServiceImpl.java
@@ -1,16 +1,21 @@
 package com.xqc.campusshop.service.impl;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.xqc.campusshop.dao.HeadLineDao;
 import com.xqc.campusshop.dto.HeadLineExecution;
 import com.xqc.campusshop.entity.HeadLine;
+import com.xqc.campusshop.enums.HeadLineStateEnum;
 import com.xqc.campusshop.service.HeadLineService;
+import com.xqc.campusshop.util.FileUtil;
+import com.xqc.campusshop.util.ImageUtil;
 
 /**
  * 头条操作实现类
@@ -32,27 +37,114 @@ public class HeadLineServiceImpl implements HeadLineService {
 	}
 
 	@Override
+	@Transactional
 	public HeadLineExecution addHeadLine(HeadLine headLine, CommonsMultipartFile thumbnail) {
 		// TODO Auto-generated method stub
-		return null;
+		if (headLine != null) {
+			headLine.setCreateTime(new Date());
+			headLine.setLastEditTime(new Date());
+			if (thumbnail != null) {
+				addThumbnail(headLine, thumbnail);
+			}
+			try {
+				int effectedNum = headLineDao.insertHeadLine(headLine);
+				if (effectedNum > 0) {
+					return new HeadLineExecution(HeadLineStateEnum.SUCCESS,
+							headLine);
+				} else {
+					return new HeadLineExecution(HeadLineStateEnum.INNER_ERROR);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("添加区域信息失败:" + e.toString());
+			}
+		} else {
+			return new HeadLineExecution(HeadLineStateEnum.EMPTY);
+		}
 	}
 
 	@Override
+	@Transactional
 	public HeadLineExecution modifyHeadLine(HeadLine headLine, CommonsMultipartFile thumbnail) {
-		// TODO Auto-generated method stub
-		return null;
+		if (headLine.getLineId() != null && headLine.getLineId() > 0) {
+			headLine.setLastEditTime(new Date());
+			if (thumbnail != null) {
+				HeadLine tempHeadLine = headLineDao.queryHeadLineById(headLine
+						.getLineId());
+				if (tempHeadLine.getLineImg() != null) {
+					FileUtil.deleteFile(tempHeadLine.getLineImg());
+				}
+				addThumbnail(headLine, thumbnail);
+			}
+			try {
+				int effectedNum = headLineDao.updateHeadLine(headLine);
+				if (effectedNum > 0) {
+					return new HeadLineExecution(HeadLineStateEnum.SUCCESS,
+							headLine);
+				} else {
+					return new HeadLineExecution(HeadLineStateEnum.INNER_ERROR);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("更新头条信息失败:" + e.toString());
+			}
+		} else {
+			return new HeadLineExecution(HeadLineStateEnum.EMPTY);
+		}
 	}
 
 	@Override
+	@Transactional
 	public HeadLineExecution removeHeadLine(long headLineId) {
-		// TODO Auto-generated method stub
-		return null;
+		if (headLineId > 0) {
+			try {
+				HeadLine tempHeadLine = headLineDao
+						.queryHeadLineById(headLineId);
+				if (tempHeadLine.getLineImg() != null) {
+					FileUtil.deleteFile(tempHeadLine.getLineImg());
+				}
+				int effectedNum = headLineDao.deleteHeadLine(headLineId);
+				if (effectedNum > 0) {
+					return new HeadLineExecution(HeadLineStateEnum.SUCCESS);
+				} else {
+					return new HeadLineExecution(HeadLineStateEnum.INNER_ERROR);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("删除头条信息失败:" + e.toString());
+			}
+		} else {
+			return new HeadLineExecution(HeadLineStateEnum.EMPTY);
+		}
 	}
 
 	@Override
 	public HeadLineExecution removeHeadLineList(List<Long> headLineIdList) {
-		// TODO Auto-generated method stub
-		return null;
+		if (headLineIdList != null && headLineIdList.size() > 0) {
+			try {
+				List<HeadLine> headLineList = headLineDao
+						.queryHeadLineByIds(headLineIdList);
+				for (HeadLine headLine : headLineList) {
+					if (headLine.getLineImg() != null) {
+						FileUtil.deleteFile(headLine.getLineImg());
+					}
+				}
+				int effectedNum = headLineDao
+						.batchDeleteHeadLine(headLineIdList);
+				if (effectedNum > 0) {
+					return new HeadLineExecution(HeadLineStateEnum.SUCCESS);
+				} else {
+					return new HeadLineExecution(HeadLineStateEnum.INNER_ERROR);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("删除头条信息失败:" + e.toString());
+			}
+		} else {
+			return new HeadLineExecution(HeadLineStateEnum.EMPTY);
+		}
+	}
+	
+	private void addThumbnail(HeadLine headLine, CommonsMultipartFile thumbnail) {
+		String dest = FileUtil.getHeadLineImagePath();
+		String thumbnailAddr = ImageUtil.generateNormalImg(thumbnail, dest);
+		headLine.setLineImg(thumbnailAddr);
 	}
 
 
