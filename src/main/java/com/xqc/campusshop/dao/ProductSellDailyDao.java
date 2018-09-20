8c6910c15472f88ea113eb146f05a73779643428
TeacherCangNulling <3218870799@qq.com>
2018-09-20 22:39:40 +0800

    产品销量统计数据访问层

---
+++ src/main/java/com/xqc/campusshop/dao/ProductSellDailyDao.java
@@ -0,0 +1,29 @@
+package com.xqc.campusshop.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.xqc.campusshop.entity.ProductSellDaily;
+
+public interface ProductSellDailyDao {
+	
+	/**
+	 * 根据查询条件返回商品日销量的统计列表
+	 * @param productSellDailyCondition
+	 * @param beginTime
+	 * @param endTime
+	 * @return
+	 */
+	List<ProductSellDaily> queryProductSellDailyList(
+			@Param("productSellDailyCondition") ProductSellDaily productSellDailyCondition,
+			@Param("beginTime") Date beginTime,@Param("endTime") Date endTime);
+
+	/**
+	 * 统计平台所有商品的日销售量
+	 * @return
+	 */
+	int insertProductSellDaily();
+	
+}
