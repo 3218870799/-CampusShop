d694e75af3b58d105ddcd9c8a016c290eb9e97d2
TeacherCangNulling <3218870799@qq.com>
2018-09-20 20:13:26 +0800

    顾客消费的商品数据访问层

---
+++ src/main/java/com/xqc/campusshop/dao/UserProductMapDao.java
@@ -0,0 +1,40 @@
+package com.xqc.campusshop.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.xqc.campusshop.entity.UserProductMap;
+
+/**
+ * 顾客消费的商品数据访问接口
+ * @author A Cang（xqc）
+ *
+ */
+public interface UserProductMapDao {
+	/**
+	 * 
+	 * @param userProductCondition
+	 * @param rowIndex
+	 * @param pageSize
+	 * @return
+	 */
+	List<UserProductMap> queryUserProductMapList(
+			@Param("userProductCondition") UserProductMap userProductCondition,
+			@Param("rowIndex") int rowIndex, @Param("pageSize") int pageSize);
+
+	/**
+	 * 
+	 * @param userProductCondition
+	 * @return
+	 */
+	int queryUserProductMapCount(
+			@Param("userProductCondition") UserProductMap userProductCondition);
+
+	/**
+	 * 
+	 * @param userProductMap
+	 * @return
+	 */
+	int insertUserProductMap(UserProductMap userProductMap);
+}
