a1aba507d8089e35496772970321cd30ae319346
TeacherCangNulling <3218870799@qq.com>
2018-09-20 22:39:03 +0800

    产品销量映射实体类

---
+++ src/main/java/com/xqc/campusshop/entity/ProductSellDaily.java
@@ -0,0 +1,46 @@
+package com.xqc.campusshop.entity;
+
+import java.util.Date;
+
+/**
+ * 顾客消费的商品映射
+ * @author A Cang（xqc）
+ *
+ */
+public class ProductSellDaily {
+	//某一天的销量
+	private Date createTime;
+	//销量
+	private Integer total;
+	//商品实体类
+	private Product product;
+	//店铺信息实体类
+	private Shop shop;
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	public Integer getTotal() {
+		return total;
+	}
+	public void setTotal(Integer total) {
+		this.total = total;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	public Shop getShop() {
+		return shop;
+	}
+	public void setShop(Shop shop) {
+		this.shop = shop;
+	}
+	
+	
+
+}
