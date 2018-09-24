794f4acbb4d90d8be50d10540d7d291dcfe23f42
TeacherCangNulling <3218870799@qq.com>
2018-09-24 15:19:35 +0800

    用户信息Service实现

--- src/main/java/com/xqc/campusshop/service/impl/PersonInfoServiceImpl.java
+++ src/main/java/com/xqc/campusshop/service/impl/PersonInfoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.xqc.campusshop.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.xqc.campusshop.dao.PersonInfoDao;
+import com.xqc.campusshop.dto.PersonInfoExecution;
 import com.xqc.campusshop.entity.PersonInfo;
+import com.xqc.campusshop.enums.PersonInfoStateEnum;
 import com.xqc.campusshop.service.PersonInfoService;
+import com.xqc.campusshop.util.PageCalculator;
 
 /**
  * 用户信息实现
@@ -20,5 +26,70 @@ public class PersonInfoServiceImpl implements PersonInfoService {
 	@Override
 	public PersonInfo getPersonInfoById(Long userId) {
 		return personInfoDao.queryPersonInfoById(userId);
+	}
+
+	@Override
+	public PersonInfoExecution getPersonInfoList(
+			PersonInfo personInfoCondition, int pageIndex, int pageSize) {
+		int rowIndex = PageCalculator.calculateRowIndex(pageIndex, pageSize);
+		List<PersonInfo> personInfoList = personInfoDao.queryPersonInfoList(
+				personInfoCondition, rowIndex, pageSize);
+		int count = personInfoDao.queryPersonInfoCount(personInfoCondition);
+		PersonInfoExecution se = new PersonInfoExecution();
+		if (personInfoList != null) {
+			se.setPersonInfoList(personInfoList);
+			se.setCount(count);
+		} else {
+			se.setState(PersonInfoStateEnum.INNER_ERROR.getState());
+		}
+		return se;
+	}
+
+	@Override
+	@Transactional
+	public PersonInfoExecution addPersonInfo(PersonInfo personInfo) {
+		if (personInfo == null) {
+			return new PersonInfoExecution(PersonInfoStateEnum.EMPTY);
+		} else {
+			try {
+				int effectedNum = personInfoDao.insertPersonInfo(personInfo);
+				if (effectedNum <= 0) {
+					return new PersonInfoExecution(
+							PersonInfoStateEnum.INNER_ERROR);
+				} else {// 创建成功
+					personInfo = personInfoDao.queryPersonInfoById(personInfo
+							.getUserId());
+					return new PersonInfoExecution(PersonInfoStateEnum.SUCCESS,
+							personInfo);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("addPersonInfo error: "
+						+ e.getMessage());
+			}
+		}
+	}
+
+	@Override
+	@Transactional
+	public PersonInfoExecution modifyPersonInfo(PersonInfo personInfo) {
+		if (personInfo == null || personInfo.getUserId() == null) {
+			return new PersonInfoExecution(PersonInfoStateEnum.EMPTY);
+		} else {
+			try {
+				int effectedNum = personInfoDao.updatePersonInfo(personInfo);
+				if (effectedNum <= 0) {
+					return new PersonInfoExecution(
+							PersonInfoStateEnum.INNER_ERROR);
+				} else {// 创建成功
+					personInfo = personInfoDao.queryPersonInfoById(personInfo
+							.getUserId());
+					return new PersonInfoExecution(PersonInfoStateEnum.SUCCESS,
+							personInfo);
+				}
+			} catch (Exception e) {
+				throw new RuntimeException("updatePersonInfo error: "
+						+ e.getMessage());
+			}
+		}
 	}
 }
