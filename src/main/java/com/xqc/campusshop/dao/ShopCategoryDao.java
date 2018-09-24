3d73d86b4df094009343687d177a0b5ef141b41b
TeacherCangNulling <3218870799@qq.com>
2018-09-24 14:41:22 +0800

    完善商品类别数据访问层

--- src/main/java/com/xqc/campusshop/dao/ShopCategoryDao.java
+++ src/main/java/com/xqc/campusshop/dao/ShopCategoryDao.java
@@ -19,4 +19,47 @@ public interface ShopCategoryDao {
 	 */
 	List<ShopCategory> queryShopCategory(@Param("shopCategoryCondition")
 	ShopCategory shopCategory);
+	
+	/**
+	 * 通过类别Id查询单个店铺类别
+	 * @param shopCategoryId
+	 * @return
+	 */
+	ShopCategory queryShopCategoryById(long shopCategoryId);
+
+	/**
+	 * 通过类别Id查询多个商品类别
+	 * @param shopCategoryIdList
+	 * @return
+	 */
+	List<ShopCategory> queryShopCategoryByIds(List<Long> shopCategoryIdList);
+
+	/**
+	 * 插入商品类别
+	 * @param shopCategory
+	 * @return
+	 */
+	int insertShopCategory(ShopCategory shopCategory);
+
+	/**
+	 * 修改商品类别信息
+	 * @param shopCategory
+	 * @return
+	 */
+	int updateShopCategory(ShopCategory shopCategory);
+
+	/**
+	 * 删除商品类别信息
+	 * @param shopCategoryId
+	 * @return
+	 */
+	int deleteShopCategory(long shopCategoryId);
+
+	/**
+	 * 批量删除
+	 * @param shopCategoryIdList
+	 * @return
+	 */
+	int batchDeleteShopCategory(List<Long> shopCategoryIdList);
+	
 }
